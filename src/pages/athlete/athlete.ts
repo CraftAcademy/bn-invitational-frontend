c346309601a059ae34cc7f6daa641fc96b7b0f73
bollen <david.boll@me.com>
2018-04-05 11:31:42 +0200

    Adds WheelSelector

--- src/pages/athlete/athlete.ts
+++ src/pages/athlete/athlete.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AthletesPage } from '../athletes/athletes';
 import { AthletesProvider } from '../../providers/athletes/athletes';
+import { WheelSelector } from '@ionic-native/wheel-selector';
 
 @IonicPage()
 @Component({
@@ -12,7 +13,38 @@ export class AthletePage {
 
   athlete: any[];
 
-  constructor(private athletesProvider: AthletesProvider, private navCtrl: NavController, private navParams: NavParams) {
+  jsonData = {
+    numbers: [
+     { description: "1" },
+      { description: "2" },
+      { description: "3" }
+    ],
+    fruits: [
+      { description: "Apple" },
+      { description: "Banana" },
+      { description: "Tangerine" }
+    ],
+    firstNames: [
+      { name: "Fred", id: '1' },
+      { name: "Jane", id: '2' },
+      { name: "Bob", id: '3' },
+      { name: "Earl", id: '4' },
+      { name: "Eunice", id: '5' }
+    ],
+    lastNames: [
+      { name: "Johnson", id: '100' },
+      { name: "Doe", id: '101' },
+      { name: "Kinishiwa", id: '102' },
+      { name: "Gordon", id: '103' },
+      { name: "Smith", id: '104' }
+    ]
+  };
+
+  constructor(private athletesProvider: AthletesProvider, 
+              private navCtrl: NavController, 
+              private navParams: NavParams,
+              private selector: WheelSelector) {
+
     this.athletesProvider
         .show(this.navParams.get('url'))             
         .subscribe(athlete => {
@@ -22,9 +54,11 @@ export class AthletePage {
              });
   }
 
+  
+
   ionViewDidLoad() {
     let url = this.navParams.get('url'); 
-    console.log(`this is the url: ${url}`);
+    console.log(this.jsonData);
   }
 
 }
