01f0c1448bbb5b27c834e7ad8fae1a32b00381bb
bollen <david.boll@me.com>
2018-04-07 11:32:08 +0200

    Adds cofig to splash.ts Creates the splash on app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, Modal, ModalController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { SplashPage } from '../pages/splash/splash';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
@@ -11,12 +12,13 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, modalCtrl: ModalController) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+      
       statusBar.styleDefault();
-      // splashScreen.hide();
+
+      let splash = modalCtrl.create(SplashPage);
+      splash.present();
     });
   }
 }
--- src/pages/splash/splash.ts
+++ src/pages/splash/splash.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ViewController } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
-@IonicPage()
+
+
 @Component({
   selector: 'page-splash',
   templateUrl: 'splash.html',
 })
 export class SplashPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public viewCtrl: ViewController, public splashScreen: SplashScreen) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SplashPage');
+    this.splashScreen.hide();
+
+    setTimeout(() => {
+      this.viewCtrl.dismiss();
+    }, 4000);
   }
 
 }
