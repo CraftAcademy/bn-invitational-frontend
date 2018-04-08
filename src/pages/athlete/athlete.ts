82e264c5d959c1536eef1c5df3c9ed076419487b
bollen <david.boll@me.com>
2018-04-08 18:49:37 +0200

    Cleans up code

--- src/pages/athlete/athlete.ts
+++ src/pages/athlete/athlete.ts
@@ -17,7 +17,7 @@ export class AthletePage {
   hasVoted: boolean = false;
   message: any;
   theValues: [string];
-
+  
   constructor(private athletesProvider: AthletesProvider,
     private navCtrl: NavController,
     private alertCtrl: AlertController,
