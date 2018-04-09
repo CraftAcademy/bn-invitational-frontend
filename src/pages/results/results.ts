2bf1c7abc14e5df6df43ee4b3ea1f7ec9e871ba3
larathordar <lara-thordar@hotmail.com>
2018-04-09 21:45:12 +0200

    removes results page import

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpModule } from '@angular/http';
 import { resolvePtr } from 'dns';
-import { ResultsPage } from '../results/results';
 import { ResultsProvider } from '../../providers/results/results';
 
 @IonicPage()
