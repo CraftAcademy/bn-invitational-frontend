f11b12ca608259658ec096b3d94fcdc530881e32
bollen <david.boll@me.com>
2018-04-10 17:40:00 +0200

    Adds alertCtrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform, Modal, ModalController } from 'ionic-angular';
+import { Platform, Modal, ModalController, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { OneSignal } from '@ionic-native/onesignal';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,12 +13,19 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, private oneSignal: OneSignal, statusBar: StatusBar, splashScreen: SplashScreen, modalCtrl: ModalController) {
+  constructor(platform: Platform, 
+      private oneSignal: OneSignal, 
+              statusBar: StatusBar, 
+              splashScreen: SplashScreen, 
+              modalCtrl: ModalController,
+      private alertCtrl: AlertController) {
     platform.ready().then(() => {
       
       let splash = modalCtrl.create(SplashPage);
       splash.present();
-      this.setupPush;
+      // if(platform.is('cordova')) {
+        this.setupPush();
+      // }
     });
 
   }
@@ -26,6 +33,29 @@ export class MyApp {
   setupPush() {
     this.oneSignal.startInit('316f8be0-f2fa-4323-9d04-86a62f80dac4');
 
+    this.oneSignal.handleNotificationReceived().subscribe(data => {
+      console.log('We recieved a push:', data);
+    });
+
+    this.oneSignal.handleNotificationOpened().subscribe(data => {
+      console.log('We opened a push:', data);
+
+      let title = data.notification.payload.title;
+      let message = data.notification.payload.body;
+
+      let alert = this.alertCtrl.create({
+        title: title,
+        subTitle: message,
+        buttons: [
+          {
+            text: 'Close',
+            role: 'cancel'
+          }
+        ]
+
+      })
+      alert.present();
+    });
     this.oneSignal.endInit();
 
   }
