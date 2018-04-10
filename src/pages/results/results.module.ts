94ca1ba89c5d4288fecd5bf55d132b27393d8896
kalle <kmthorsnes@gmail.com>
2018-04-10 15:33:52 +0200

    Adds css classes and styles result page

--- src/pages/results/results.module.ts
+++ src/pages/results/results.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ResultsPage } from '../results/results';
+import { AthletesPage } from '.athletes/athletes';
 
 @NgModule({
   declarations: [
