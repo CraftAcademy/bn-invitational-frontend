46030a48101f02f05261d181dafef371d63603ac
kalle <kmthorsnes@gmail.com>
2018-04-10 16:04:53 +0200

    work in project

--- src/pages/results/results.module.ts
+++ src/pages/results/results.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ResultsPage } from '../results/results';
-import { AthletesPage } from '.athletes/athletes';
 
 @NgModule({
   declarations: [
