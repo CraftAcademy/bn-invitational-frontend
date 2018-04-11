16902e2b3005a48b7012c0bbf724126763b63b4f
bollen <david.boll@me.com>
2018-04-11 11:09:56 +0200

    Adds inFocusDisplaying parameter for push-notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,26 +35,20 @@ export class MyApp {
   
   setupPush() {
     this.oneSignal.startInit('316f8be0-f2fa-4323-9d04-86a62f80dac4');
-
-    // this.oneSignal.handleNotificationReceived().subscribe(data => {
-    //   console.log('We recieved a push:', data);
-    // });
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
 
     this.oneSignal.handleNotificationOpened().subscribe(data => {
       console.log('We opened a push:', data);
 
-      // let title = data.notification.payload.title;
-      // let message = data.notification.payload.body;
-
-      // let action = data.notification.payload.additionalData['action'];
+      let action = data.notification.payload.additionalData['action'];
       let actionId = data.notification.payload.additionalData['id'];
       
-      this.nav.push(AthletePage, { athlete_id: actionId });
-      
+      if(action === "openPage") {
+        this.nav.push(AthletePage, { athlete_id: actionId });
+      } 
       
     });
     this.oneSignal.endInit();
 
   }
-
 }
