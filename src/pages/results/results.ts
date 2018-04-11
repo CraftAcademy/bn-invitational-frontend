70e627f1bd9f95eedbdf969d135105cbaef9b613
Holger_Tidemand <tidemand.holger@gmail.com>
2018-04-11 20:17:16 +0200

    adds check if result is ready to be published

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -11,18 +11,30 @@ export class ResultsPage {
 
   validResults: any = []
   invalidResults: any = []
+  readyToPublish:number = 1;
+  finalStanding:boolean = false
 
   constructor(private resultsProvider: ResultsProvider) {
     this.resultsProvider
         .all()
         .subscribe(results => {
 
-      console.log(results.data[0].attributes.validscore)
-       for( var i:number = 0; i < results.data.length; i++) {
-          if (results.data[i].attributes.validscore == true ){
-            this.validResults.push(results.data[i])
-          } else {
-            this.invalidResults.push(results.data[i])
+        for (var j:number = 0; j < results.data.length; j++){
+          if(results.data[j].attributes.validscore == true){
+            this.readyToPublish++ 
+            console.log(j)
+          }
+        }
+        console.log(this.readyToPublish)
+        console.log(results.data.length)
+        if(this.readyToPublish == results.data.length){
+          this.finalStanding = true
+          for( var i:number = 0; i < results.data.length; i++) {
+            if (results.data[i].attributes.validscore == true ){
+              this.validResults.push(results.data[i])
+            } else {
+              this.invalidResults.push(results.data[i])
+            }
           }
         }
       }
