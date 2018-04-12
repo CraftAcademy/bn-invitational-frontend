de9fe67445a73bc272ee3614ec290fce3b23856b
bollen <david.boll@me.com>
2018-04-12 10:10:01 +0200

    Removes splash-page Adds back check if platfrom is cordova in app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,34 +6,29 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { AthletePage } from '../pages/athlete/athlete';
 import { ResultsPage } from '../pages/results/results';
-import { SplashPage } from '../pages/splash/splash';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
   @ViewChild(Nav) nav;
 
-  constructor(platform: Platform, 
-      private oneSignal: OneSignal, 
-              statusBar: StatusBar, 
-              splashScreen: SplashScreen, 
-              modalCtrl: ModalController) {
+  constructor(platform: Platform,
+    private oneSignal: OneSignal,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    modalCtrl: ModalController) {
 
     platform.ready().then(() => {
-      
-      let splash = modalCtrl.create(SplashPage);
-      splash.present();
-      
+      if (platform.is('cordova')) {
         this.setupPush();
-      
+      }
     });
-
   }
-  
+
   setupPush() {
     this.oneSignal.startInit('316f8be0-f2fa-4323-9d04-86a62f80dac4', '203539919772');
     this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
@@ -43,12 +38,12 @@ export class MyApp {
 
       let action = data.notification.payload.additionalData['action'];
       let actionId = data.notification.payload.additionalData['id'];
-      
-      if(action === "openPage") {
-        this.nav.push(AthletePage, { athlete_id: actionId });
-      } 
 
-      else if(action === "openResults") {
+      if (action === "openPage") {
+        this.nav.push(AthletePage, { athlete_id: actionId });
+      }
+
+      else if (action === "openResults") {
         this.nav.push(ResultsPage);
       }
 
